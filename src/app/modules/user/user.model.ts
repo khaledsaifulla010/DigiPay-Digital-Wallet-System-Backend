c164cff2dee7bd7e1d1e12f0db94bfc7a5876df2
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-08-05 10:42:11 +0600

    create user model

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+import { IUser } from "./user.interface";
+
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["USER", "AGENT", "ADMIN"],
+      default: "USER",
+    },
+    status: {
+      type: String,
+      enum: ["ACTIVE", "BLOCKED"],
+      default: "ACTIVE",
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export const User = model<IUser>("User", userSchema);
