d9b5b58f8f166432bff92b01b3d104815db7e03f
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2025-08-09 10:28:56 +0600

    setup passport for custom authentication

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -7,6 +7,10 @@ interface EnvConfig {
   DATABASE_URL: string;
   NODE_ENV: string;
   BCRYPT_SALT_ROUND: string;
+  JWT_ACCESS_SECRET: string;
+  JWT_ACCESS_EXPIRES: string;
+  JWT_REFRESH_SECRET: string;
+  JWT_REFRESH_EXPIRES: string;
 }
 const loadEnvVariables = (): EnvConfig => {
   const requiredEnvVariables: string[] = [
@@ -14,6 +18,10 @@ const loadEnvVariables = (): EnvConfig => {
     "DATABASE_URL",
     "NODE_ENV",
     "BCRYPT_SALT_ROUND",
+    "JWT_ACCESS_SECRET",
+    "JWT_ACCESS_EXPIRES",
+    "JWT_REFRESH_SECRET",
+    "JWT_REFRESH_EXPIRES",
   ];
   requiredEnvVariables.forEach((key) => {
     if (!process.env[key]) {
@@ -25,6 +33,10 @@ const loadEnvVariables = (): EnvConfig => {
     DATABASE_URL: process.env.DATABASE_URL as string,
     NODE_ENV: process.env.NODE_ENV as string,
     BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+    JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+    JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
   };
 };
 
